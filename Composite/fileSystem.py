f9b24d8f188f83206065c0256d4aa00e98b9f1b1
Richard Sterling <rsterlin@mavs.coloradomesa.edu>
2017-03-31 13:35:05 -0600

    fixed bug in fileSystem.py

--- Composite/fileSystem.py
+++ Composite/fileSystem.py
@@ -30,8 +30,10 @@ class Component:
 
 
     def Add(self, item, destination):
+        print(str(type(item)))
         try:
-            item = eval(item)
+            if isinstance(item, str):
+                item = eval(item)
             if type(item) == Folder:
                 item.name = item.name.upper()
             try:
